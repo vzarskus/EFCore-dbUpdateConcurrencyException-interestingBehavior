df7093e396268d78f7bd7f8f1eb240f55cda591d
Vytautas <vytautasz@eldorado.gg>
2023-01-05 16:20:48 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ public class Product
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    [ConcurrencyCheck]
     public string Description { get; set; }
     [ConcurrencyCheck]
     public decimal Price { get; set; }
